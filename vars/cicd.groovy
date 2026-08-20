dd3ac955bf25161f950bdb3d686fa5bfab3d4e2c
Mone Kusuma <monekusuma3121@gmail.com>
2026-08-21 02:07:46 +0530

    Refactor functions in cicd.groovy for consistency

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,14 +1,16 @@
-def gitDownload(repo)
-{
-  git "https://github.com/IntelliqDevops/${repo}.git"
+def gitDownload(repo) {
+    git "https://github.com/IntelliqDevops/${repo}.git"
 }
-def buildArtifact(){
+
+def buildArtifact() {
     sh "mvn package"
 }
-def deploytoTomcat(ip,context){
-  sh "scp ${WORKSPACE}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"  
+
+def deployToTomcat(ip, context) {
+    sh "scp ${WORKSPACE}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
-def runSelenium(){
-  sh "java -jar ${WORKSPACE}/testing.jar"
+
+def runSelenium() {
+    sh "java -jar ${WORKSPACE}/testing.jar"
 }
 
