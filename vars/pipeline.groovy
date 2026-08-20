409c3303b3e8a292bf02a2faa80084aa8c376ab0
Mone Kusuma <monekusuma3121@gmail.com>
2026-08-21 00:52:43 +0530

    Add pipeline functions for git, build, and deploy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,14 @@
+def gitDownload(repo)
+{
+  git "https://github.com/IntelliqDevops/${repo}.git"
+}
+def buildArtifact(){
+    sh "mvn package"
+}
+def deploytoTomcat(ip,context){
+  sh "scp ${WORKSPACE}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"  
+}
+def runSelenium(){
+  sh "java -jar ${WORKSPACE}/testing.jar"
+}
+
